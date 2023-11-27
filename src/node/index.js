ae071c2e1ed963de50e068f16decda57e39c7a49
大塚 <shoma.otsuka@rakus-partners.co.jp>
2023-11-27 14:29:15 +0900

    27日

--- src/node/index.js
+++ src/node/index.js
@@ -2,17 +2,18 @@ const express = require("express");
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 
-const port = 自分の社員番号;
+const port = 3522
+;
 
 const cors = require("cors");
 app.use(cors());
 
 const { Pool } = require("pg");
 const pool = new Pool({
-  user: "x", // PostgreSQLのユーザー名に置き換えてください
-  host: "localhost",
-  database: "x", // PostgreSQLのデータベース名に置き換えてください
-  password: "x", // PostgreSQLのパスワードに置き換えてください
+  user: "user_3522", // PostgreSQLのユーザー名に置き換えてください
+  host: "db",
+  database: "crm_3522", // PostgreSQLのデータベース名に置き換えてください
+  password: "pass_3522", // PostgreSQLのパスワードに置き換えてください
   port: 5432,
 });
 
@@ -37,7 +38,7 @@ app.post("/add-customer", async (req, res) => {
   try {
     const { companyName, industry, contact, location } = req.body;
     const newCustomer = await pool.query(
-      "INSERT INTO customers (company_nam, industry, contact, location) VALUES ($1, $2, $3, $4) RETURNING *",
+      "INSERT INTO customers (company_name, industry, contact, location) VALUES ($1, $2, $3, $4) RETURNING *",
       [companyName, industry, contact, location]
     );
     res.json({ success: true, customer: newCustomer.rows[0] });
